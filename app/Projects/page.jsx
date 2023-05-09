a5ef84dcc8f09c2cbac3bd72f9412b890bf93bd2
ssimona03 <sstroe92@gmail.com>
2023-05-09 22:00:03 +0100

    update

--- app/Projects/page.jsx
+++ app/Projects/page.jsx
@@ -11,28 +11,32 @@ const projectsData = [
         title: "Food APP",
         src: '/food-app.png',
         description: "Food APP is a work in progress aplication that will show you what foods you can eat based on any of the symptoms for IBS, Gastritis/Acid Reflux and Gallbalder. In the app you can select multiple diets and once you log in you can edit the results. The application is using: Html, CSS, Javascript, React, Node 13, MongoDB, Firebase, Tailwind.",
-        github: "https://github.com/SSimona03/food-app"
+        github: "https://github.com/SSimona03/food-app",
+        website: "https://s-food-app.vercel.app"
     },
     {
         id: 2,
         title: "Meme Generator",
         src : "/meme.png",
         description: " The app is created using HTML, CSS, Javascript and React. It works by randomly selecting a meme image and you can add top and/or bottom text to it.",
-        github: "https://github.com/SSimona03/react/tree/main/react-memeGenerator-03"
+        github: "https://github.com/SSimona03/react/tree/main/react-memeGenerator-03",
+        website: "/"
     },
     {
         id: 3,
         title: "Travel Journal",
         src: "/travel-journal.png",
         description: "Is a journal website that containes a collection of cities and the travel details. It was created using HTML, CSS, Javascript and React.",
-        github: "https://github.com/SSimona03/react/tree/main/react-travel-journal-01"
+        github: "https://github.com/SSimona03/react/tree/main/react-travel-journal-01",
+        website: "/"
     },
     {
         id: 4,
-        titile: "Tenzies Game",
+        title: "Tenzies Game",
         src:  "/tenzies-game.png",
         description: "The objective of the game is to have all the dice with the same number. It was created using HTML, CSS, Javascript and React.",
-        github: "https://github.com/SSimona03/react/tree/main/react-game-app04"
+        github: "https://github.com/SSimona03/react/tree/main/react-game-app04",
+        website: "/"
     }
 ]
 
@@ -49,10 +53,12 @@ const projectsData = [
                 projectsData.map(x => (
                     <div key={x.id} className="prod-section"  >
                         <div className="prod-img">
+                            <Link target="_blank" href={x.website}> 
                             <Image  className= "test"src={x.src} alt="project-image" width={550}
                             height={200}></Image>
+                            </Link>
                         </div>
-                        <div className="prod-details pt-6">
+                        <div className="prod-details pt-10">
                             <h1 className="text-xl font-bold pb-4">{x.title}</h1>
                             <p className="">{x.description}</p>
                                 <div className="hero-btn-section pt-10">
