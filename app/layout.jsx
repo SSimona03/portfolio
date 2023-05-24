201121dde3314f8d6e325c03846377953b4f8f94
ssimona03 <sstroe92@gmail.com>
2023-05-24 13:19:19 +0100

    add analitics

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,6 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+         <Analytics />
       <body className= {`${sourceCodePro.className}`}>
       <Navbar />
       {children}
@@ -22,7 +23,6 @@ export default function RootLayout({ children }) {
       <Projects />
       <Footer />
       </body>
-      <Analytics />
     </html>
   )
 }
